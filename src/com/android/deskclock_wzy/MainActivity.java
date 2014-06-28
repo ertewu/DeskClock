171abb38bef49e214f932c2bc5f1c76576c9a32d
ertewu <ertewu@163.com>
2014-06-28 23:32:05 +0800

    工程加入git

---
+++ src/com/android/deskclock_wzy/MainActivity.java
@@ -0,0 +1,22 @@
+package com.android.deskclock_wzy;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_activity);
+    }
+
+    public void onBtn1Click(View view) {
+
+    }
+
+    public void onBtn2Click(View view) {
+
+    }
+}
